b58cb34ba1c5608813ad01fd98a79a5bdfaf2e0e
Danil <keepfocused@users.noreply.github.com>
2017-12-18 17:05:52 +0300

    Initial commit

---
+++ Pods/Target Support Files/Pods-More Kuponov/Pods-More Kuponov-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_More_Kuponov : NSObject
+@end
+@implementation PodsDummy_Pods_More_Kuponov
+@end
---
+++ ServerManager.swift
@@ -0,0 +1,14 @@
+//
+//  ServerManager.swift
+//  More Kuponov
+//
+//  Created by Danil on 15.12.17.
+//  Copyright © 2017 MoreKuponov. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class ServerManager:Alamofire {
+    
+}
